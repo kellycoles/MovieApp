3be12394affdfe3edc867e254e3d262a9a9425b4
kellycoles <kellycoles1@gmail.com>
2020-02-15 17:09:08 -0600

    nav link to movies

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -9,6 +9,7 @@ namespace RazorPagesMovie.Models
         public string Title { get; set; }
 
         [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
