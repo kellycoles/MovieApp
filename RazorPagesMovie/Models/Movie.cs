d59b8aac13d466747940df0f0719fbf747160fb3
kellycoles <kellycoles1@gmail.com>
2020-02-15 18:20:16 -0600

    added route template

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
@@ -12,6 +13,7 @@ namespace RazorPagesMovie.Models
         [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
